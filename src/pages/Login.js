1c08946634a6032169277660aa98afb30ea2325d
Grant Williams <grant.austin.williams@gmail.com>
2019-03-20 11:01:47 -0500

    Updated Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import {
-  Alert,
   Button,
   Card,
   CardBody,
@@ -8,11 +7,12 @@ import {
   CardFooter,
   Form,
   FormGroup,
-  // FormFeedback,
+  FormFeedback,
   Input,
   InputGroup,
   InputGroupAddon,
-  InputGroupText
+  InputGroupText,
+  UncontrolledAlert
 } from "reactstrap";
 import axios from "axios";
 
@@ -25,33 +25,14 @@ class Login extends Component {
       email: "",
       password: "",
       validate: {
-        emailState: "has-success",
-        passwordState: "has-success"
+        emailState: "",
+        passwordState: ""
       },
       loginValidated: false,
-      alertSuccessVisible: false,
-      alertSuccessMessage: ""
+      alertVisible: false
     };
     this.handleChange = this.handleChange.bind(this);
-    this.onDismiss = this.onDismiss.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  onDismiss() {
-    //allows successful login alert to be dismissed
-    this.setState({ alertSuccessVisible: false });
-  }
-
-  setAlertMessage() {
-    //sets the message to be displayed to user on successful login
-    let submitString =
-      // `[Full Name]: ${this.state.fullname}\n` + // `[Email]: ${this.state.email}\n` +
-      // `[Password]: ${this.state.password}\n` +
-      `Login Successful, Welcome Back ${this.state.fullname}!`;
-    this.setState({
-      alertSuccessMessage: submitString,
-      alertSuccessVisible: true
-    });
   }
 
   handleChange = async event => {
@@ -64,28 +45,28 @@ class Login extends Component {
     });
   };
 
-  // validateEmail(e) {
-  //   //ensures that user input adheres for email matches requiremnts given by regular expression
-  //   const emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  //   const { validate } = this.state;
-  //   if (emailRex.test(e.target.value)) {
-  //     validate.emailState = "has-success";
-  //   } else {
-  //     validate.emailState = "has-danger";
-  //   }
-  //   this.setState({ validate });
-  // }
+  validateEmail(e) {
+    //ensures that user input adheres for email matches requiremnts given by regular expression
+    const emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const { validate } = this.state;
+    if (emailRex.test(e.target.value)) {
+      validate.emailState = "has-success";
+    } else {
+      validate.emailState = "has-danger";
+    }
+    this.setState({ validate });
+  }
 
-  // validatePassword(e) {
-  //   //ensures that user input for password is not empty
-  //   const { validate } = this.state;
-  //   if (e.target.value !== "") {
-  //     validate.passwordState = "has-success";
-  //   } else {
-  //     validate.passwordState = "has-danger";
-  //   }
-  //   this.setState({ validate });
-  // }
+  validatePassword(e) {
+    //ensures that user input for password is not empty
+    const { validate } = this.state;
+    if (e.target.value !== "") {
+      validate.passwordState = "has-success";
+    } else {
+      validate.passwordState = "has-danger";
+    }
+    this.setState({ validate });
+  }
 
   handleSubmit(event) {
     if (
@@ -101,13 +82,35 @@ class Login extends Component {
         //access and sort return codes here
       });
 
-      this.setAlertMessage(); //success message
+      this.setState({ alertVisible: true });
+    }
+  }
+
+  showAlert() {
+    const loginValidated = this.state.loginValidated;
+    if (loginValidated) {
+      return (
+        <UncontrolledAlert color="success">
+          Login Successful. Welcome Back {this.state.fullname}!
+          <Button color="link" href="/userprofile">
+            Proceed to Profile Page
+          </Button>
+        </UncontrolledAlert>
+      );
+    } else {
+      return (
+        <UncontrolledAlert color="danger">
+          Login Unsuccessful. Invalid Username and/or Password.
+        </UncontrolledAlert>
+      );
     }
   }
 
   cardStyle = { margin: "5% auto", maxWidth: "600px", minWidth: "200px" };
   coloring = { backgroundColor: "#422ef7", color: "	#ffffff" };
+
   render() {
+    const alertVisible = this.state.alertVisible;
     return (
       <div className="login">
         <Card style={this.cardStyle} outline color="dark">
@@ -132,18 +135,18 @@ class Login extends Component {
                     placeholder="Email"
                     autoComplete="email"
                     value={this.state.email}
-                    // valid={this.state.validate.emailState === "has-success"}
-                    // invalid={this.state.validate.emailState === "has-danger"}
+                    valid={this.state.validate.emailState === "has-success"}
+                    invalid={this.state.validate.emailState === "has-danger"}
                     onChange={e => {
-                      // this.validateEmail(e);
+                      this.validateEmail(e);
                       this.handleChange(e);
                     }}
                     required
                   />
                 </InputGroup>
-                {/* <FormFeedback invalid>
+                <FormFeedback invalid>
                   Please enter a valid email (example@example.com)
-                </FormFeedback> */}
+                </FormFeedback>
               </FormGroup>
               <FormGroup>
                 <InputGroup>
@@ -159,16 +162,16 @@ class Login extends Component {
                     placeholder="Password"
                     autoComplete="current-password"
                     value={this.state.password}
-                    // valid={this.state.validate.passwordState === "has-success"}
-                    // invalid={this.state.validate.passwordState === "has-danger"}
+                    valid={this.state.validate.passwordState === "has-success"}
+                    invalid={this.state.validate.passwordState === "has-danger"}
                     onChange={e => {
-                      // this.validatePassword(e);
+                      this.validatePassword(e);
                       this.handleChange(e);
                     }}
                     required
                   />
                 </InputGroup>
-                {/* <FormFeedback invalid>Please enter a password</FormFeedback> */}
+                <FormFeedback invalid>Please enter a password</FormFeedback>
               </FormGroup>
               <FormGroup className="form-actions">
                 <Button type="submit" size="sm" block color="danger">
@@ -176,16 +179,7 @@ class Login extends Component {
                 </Button>
               </FormGroup>
             </Form>
-            <Alert
-              color="success"
-              isOpen={this.state.alertSuccessVisible}
-              toggle={this.onDismiss}
-            >
-              {this.state.alertSuccessMessage}
-              <Button color="link" href="/userprofile">
-                Proceed to Profile Page
-              </Button>
-            </Alert>
+            {alertVisible ? this.showAlert() : <></>}
           </CardBody>
           <CardFooter style={{ padding: "0", backgroundColor: "#e6e6e6" }}>
             Not a Member Yet?

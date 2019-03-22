5891d28a94a76ea47a1b3dd4f2c523b42d5bcef8
Grant Williams <grant.austin.williams@gmail.com>
2019-03-22 08:28:35 -0500

    Removed inline styling from UserInternal

--- src/components/UserProfile/UserInternal.js
+++ src/components/UserProfile/UserInternal.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  Button,
-  Card,
-  CardHeader,
-  CardBody
-} from "reactstrap";
+import { Button, Card, CardHeader, CardBody } from "reactstrap";
 import ProfilePic from "../../assets/logo.svg";
 import UserInfoForm from "./Forms/UserInfoForm";
 import UserBioForm from "./Forms/UserBioForm";
@@ -17,19 +12,22 @@ class UserInternal extends React.Component {
       rating: "4.5 out of 5"
     };
   }
-  cardStyle = { margin: "0% auto", minWidth: "300px" };
-  innerCard = { margin: "5% auto", minWidth: "300px" };
-  cardHeader = { padding: ".5%", backgroundColor: "#422ef7", color: "white" };
-  cardFooter = { padding: "1%", backgroundColor: "#e6e6e6" };
+  // cardStyle = { margin: "0% auto", minWidth: "300px" };
+  // innerCard = { margin: "5% auto", minWidth: "300px" };
+  // cardHeader = { padding: ".5%", backgroundColor: "#422ef7", color: "white" };
+  // cardFooter = { padding: "1%", backgroundColor: "#e6e6e6" };
   render() {
     return (
-      <div className="userext">
-        <Card style={this.cardStyle}>
-          <CardHeader style={this.cardHeader}>
+      <>
+        {/* <Card style={this.cardStyle} */}
+        <Card className="cardStyle">
+          {/* <CardHeader style={this.cardHeader}> */}
+          <CardHeader className="cardHeader">
             User Info (Internal View)
           </CardHeader>
           <CardBody>
-            <Card style={this.innerCard}>
+            {/* <Card style={this.innerCard}> */}
+            <Card className="innerCard">
               <CardBody>
                 <img src={ProfilePic} alt="profilepic" />
                 <Button color="secondary" style={{ padding: ".5%" }} block>
@@ -39,14 +37,18 @@ class UserInternal extends React.Component {
             </Card>
             <h2>{this.state.name}</h2>
             <h6>Rating: {this.state.rating}</h6>
-            <Card style={this.innerCard}>
-              <CardHeader style={this.cardHeader}>Bio</CardHeader>
+            {/* <Card style={this.innerCard}> */}
+            <Card className="innerCard">
+              {/* <CardHeader style={this.cardHeader}> */}
+              <CardHeader className="cardHeader">Bio</CardHeader>
               <CardBody>
                 <UserBioForm />
               </CardBody>
             </Card>
-            <Card style={this.innerCard}>
-              <CardHeader style={this.cardHeader}>
+            {/* <Card style={this.innerCard}> */}
+            <Card className="innerCard">
+              {/* <CardHeader style={this.cardHeader}> */}
+              <CardHeader className="cardHeader">
                 Personal Information
               </CardHeader>
               <CardBody>
@@ -55,7 +57,7 @@ class UserInternal extends React.Component {
             </Card>
           </CardBody>
         </Card>
-      </div>
+      </>
     );
   }
 }

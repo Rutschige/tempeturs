dd7a4b38fd2495381e8741e72dff2e35b308f4f3
Grant Williams <grant.austin.williams@gmail.com>
2019-03-22 13:45:16 -0500

    Update Login.test.js

--- src/__tests__/Login.test.js
+++ src/__tests__/Login.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Login from '../pages/Login';
+import React from "react";
+import ReactDOM from "react-dom";
+import Login from "../pages/Login";
+import { shallow } from "enzyme";
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("Login shallow renders without crashing", () => {
+  shallow(<Login />);
+});
+
+it("Login renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<Login />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('Login renders correctly compared to snapshot', () => {
+  const tree = renderer
+    .create(<Login />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+})
